b32470b9da2e0dd86fc85a6c53d91767eda360e1
btrintm-beep <btrintm@gmail.com>
2026-03-22 12:31:21 -0600

    Add data loading and splitting for NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,3 +6,11 @@ from sklearn.metrics import classification_report, confusion_matrix, roc_auc_sco
 import tensorflow as tf
 from tensorflow import keras
 
+df = pd.read_csv("cardio_clean.csv")
+
+X = df.drop(["cardio"])
+Y = df["cardio"]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42, stratify = Y)
+
+print(f"Train: {X_train.shape}, Test: {X_test.shape}")
